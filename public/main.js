c8d2803454c177692334293da6191fc1f7fbc4e6
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-22 16:20:23 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,73 @@
+// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+// var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+var trash = document.getElementsByClassName("fa-trash");
+//
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('thumbUp', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+// //thumbs down
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('thumbDown', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const habit = this.parentNode.parentNode.childNodes[1].innerText
+        const whys = this.parentNode.parentNode.childNodes[3].innerText
+        const dayCount = this.parentNode.parentNode.childNodes[5].innerText
+
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'habit': habit,
+            'whys': whys,
+            'dayCount' : dayCount
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
